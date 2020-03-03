39f84ba0daf5a9d481e551116a338901823586fc
fayazsn <fayazsn@mail.uc.edu>
2020-03-03 18:37:02 -0500

    added PS folder and counters.ps1

---
+++ powershell/counters.ps1
@@ -0,0 +1,12 @@
+﻿$Machines = 'fayazsn-win'
+Foreach ($Machine in $Machines) {
+   $pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 3).CounterSamples.CookedValue
+   $sample = 1
+     foreach ($p in $pt) {
+        $date = Get-Date -Format g
+        write-host("{3} - Sample {2}: CPU is at {0} on {1} " -f [int]$p, $machine, $sample, $date) | Out-File -Append C:\logs\CPU.txt
+      $sample++
+   }
+
+
+}
