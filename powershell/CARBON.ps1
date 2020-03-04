18730ac33f71c2dd65633b6ffc94a87541cc6881
fayazsn <fayazsn@mail.uc.edu>
2020-03-03 19:43:51 -0500

    Added all of the python scripts in regards to Carbon

---
+++ powershell/CARBON.ps1
@@ -0,0 +1,16 @@
+﻿import-module 'Carbon'
+
+$OSType = Test-OSIs64Bit
+Write-Host("Testing if your OS Type is 64-Bit, the result is: $OSType")
+
+
+$InstallDiretory = Install-Directory -Path C:\IT3038C\CARBON
+Write-Host("Creating your Directory for Carbon in the IT3038C drive." )
+
+$TestFeature = Test-WindowsFeature -Name IIS-Server 
+
+Write-Host("We have just tested the IIS-Server")
+
+Write-Host("Congratulations, all three of the Carbon Functions worked :)")
+
+
