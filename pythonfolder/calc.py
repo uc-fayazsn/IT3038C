cc1b800252d3596d36e638577bf04dee1fd7d15a
fayazsn <fayazsn@mail.uc.edu>
2020-03-10 08:59:56 -0400

    updated calculator python script to include 4 more functionalities as well as a welcome screen with an update log

--- pythonfolder/calc.py
+++ pythonfolder/calc.py
@@ -1,48 +1,129 @@
+import time
+def updateLog():
+
+    print('''
+    
+    Welcome to Fayaz's IT3038C Calculator. For Project 2 the updates to this code include expotentials, remainder, and modulus functionalities as well as a way to calculation using the calculator outside of running the program. The calculator will start shortly. Enjoy!
+    
+    ''')
+
 def calculate():
-    calculation = input('''
-Please type in the math calculation you would like to complete:
+
+    operation = input('''
+
+Please type in the math operation you would like to complete:
+
 + for addition
+
 - for subtraction
+
 * for multiplication
+
 / for division
+
+** for exponents
+
+% for remainder
+
+// for floor division
+
 ''')
 
-    number_1 = int(input('Please enter the first number: '))
-    number_2 = int(input('Please enter the second number: '))
 
-    if calculation == '+':
-        print('{} + {} = '.format(number_1, number_2))
-        print(number_1 + number_2)
 
-    elif calculation == '-':
-        print('{} - {} = '.format(number_1, number_2))
-        print(number_1 - number_2)
+    inputOne = int(input('Please enter the first number: '))
 
-    elif calculation == '*':
-        print('{} * {} = '.format(number_1, number_2))
-        print(number_1 * number_2)
+    inputTwo = int(input('Please enter the second number: '))
 
-    elif calculation == '/':
-        print('{} / {} = '.format(number_1, number_2))
-        print(number_1 / number_2)
+
+
+    if operation == '+':
+
+        print('{} + {} = '.format(inputOne, inputTwo))
+
+        print(inputOne + inputTwo)
+
+
+
+    elif operation == '-':
+
+        print('{} - {} = '.format(inputOne, inputTwo))
+
+        print(inputOne - inputTwo)
+
+
+
+    elif operation == '*':
+
+        print('{} * {} = '.format(inputOne, inputTwo))
+
+        print(inputOne * inputTwo)
+
+
+
+    elif operation == '/':
+       
+        print('{} / {} = '.format(inputOne, inputTwo))
+
+        print(inputOne / inputTwo)
+  
+
+    elif operation == '**':
+      
+      print('{} ** {} = '.format(inputOne, inputTwo))
+
+      print(inputOne ** inputTwo)
+      
+    
+    elif operation == '%':
+      
+      print('{} % {} = '.format(inputOne, inputTwo))
+
+      print(inputOne % inputTwo) 
+      
+    elif operation == '//':
+      
+      print('{} // {} = '.format(inputOne, inputTwo))
+
+      print(inputOne // inputTwo) 
 
     else:
-        print('You have not typed a valid entry, please use an operator listed in the above menu.')
 
-    # Add continue() function to calculate() function
-    continue()
+        print('You have not typed a valid operator, please run the program again.')
 
-def continue():
-    calc_continue = input('''
-Do you want to calculate continue?
+
+
+    # Add onemoretime() function to calculate() function
+
+    onemoretime()
+
+
+
+def onemoretime():
+
+    calc_onemoretime = input('''
+
+Do you want to calculate another time?
+
 Please type Y for YES or N for NO.
+
 ''')
 
-    if calc_continue.upper() == 'Y':
-        calculate()
-    elif calc_continue.upper() == 'N':
-        print('See you later.')
-    else:
-        continue()
 
+
+    if calc_onemoretime.upper() == 'Y':
+
+        calculate()
+
+    elif calc_onemoretime.upper() == 'N':
+
+        print('See you later. Thank you for using this calculator.')
+
+    else:
+
+        onemoretime()
+
+
+updateLog()
+time.sleep(5)
 calculate()
